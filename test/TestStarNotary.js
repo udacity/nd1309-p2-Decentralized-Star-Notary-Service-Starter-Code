b9ad998ee03c98b7d4d45b3f017d4d01c79781c6
Berk Mollamustafaoglu <berkmolla@gmail.com>
2022-03-27 16:36:25 +0100

    Fix tests by adding approve calls to tests for ERC721.

--- test/TestStarNotary.js
+++ test/TestStarNotary.js
@@ -33,6 +33,7 @@ it('lets user1 get the funds after the sale', async() => {
     let starPrice = web3.utils.toWei(".01", "ether");
     let balance = web3.utils.toWei(".05", "ether");
     await instance.createStar('awesome star', starId, {from: user1});
+    await instance.approve(user2, starId, {from: user1});
     await instance.putStarUpForSale(starId, starPrice, {from: user1});
     let balanceOfUser1BeforeTransaction = await web3.eth.getBalance(user1);
     await instance.buyStar(starId, {from: user2, value: balance});
@@ -52,6 +53,7 @@ it('lets user2 buy a star, if it is put up for sale', async() => {
     await instance.createStar('awesome star', starId, {from: user1});
     await instance.putStarUpForSale(starId, starPrice, {from: user1});
     let balanceOfUser1BeforeTransaction = await web3.eth.getBalance(user2);
+    await instance.approve(user2, starId, {from: user1});
     await instance.buyStar(starId, {from: user2, value: balance});
     assert.equal(await instance.ownerOf.call(starId), user2);
 });
@@ -65,12 +67,27 @@ it('lets user2 buy a star and decreases its balance in ether', async() => {
     let balance = web3.utils.toWei(".05", "ether");
     await instance.createStar('awesome star', starId, {from: user1});
     await instance.putStarUpForSale(starId, starPrice, {from: user1});
-    let balanceOfUser1BeforeTransaction = await web3.eth.getBalance(user2);
-    const balanceOfUser2BeforeTransaction = await web3.eth.getBalance(user2);
-    await instance.buyStar(starId, {from: user2, value: balance, gasPrice:0});
-    const balanceAfterUser2BuysStar = await web3.eth.getBalance(user2);
-    let value = Number(balanceOfUser2BeforeTransaction) - Number(balanceAfterUser2BuysStar);
-    assert.equal(value, starPrice);
+    const balanceOfUser2BeforeTransaction = web3.utils.toBN(await web3.eth.getBalance(user2));
+    await instance.approve(user2, starId, {from: user1});
+    const txInfo = await instance.buyStar(starId, {from: user2, value: balance});
+    const balanceAfterUser2BuysStar = web3.utils.toBN(await web3.eth.getBalance(user2));    
+    
+    // Important! Note that because these are big numbers (more than Number.MAX_SAFE_INTEGER), we
+    // need to use the BN operations, instead of regular operations, which cause mathematical errors.
+    // See https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number/MAX_SAFE_INTEGER
+    // console.log("Ok  = " + (balanceOfUser2BeforeTransaction.sub(balanceAfterUser2BuysStar)).toString());
+    // console.log("Bad = " + (balanceOfUser2BeforeTransaction - balanceAfterUser2BuysStar).toString());
+
+    // calculate the gas fee
+    const tx = await web3.eth.getTransaction(txInfo.tx);
+    const gasPrice = web3.utils.toBN(tx.gasPrice);
+    const gasUsed = web3.utils.toBN(txInfo.receipt.gasUsed);
+    const txGasCost = gasPrice.mul(gasUsed);    
+
+    // make sure that [final_balance == initial_balance - star_price - gas_fee]
+    const starPriceBN = web3.utils.toBN(starPrice); // from string
+    const expectedFinalBalance = balanceOfUser2BeforeTransaction.sub(starPriceBN).sub(txGasCost);
+    assert.equal(expectedFinalBalance.toString(), balanceAfterUser2BuysStar.toString());
 });
 
 // Implement Task 2 Add supporting unit tests
