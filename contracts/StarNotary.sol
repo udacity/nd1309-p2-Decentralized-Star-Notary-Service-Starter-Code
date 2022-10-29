bf849e6ce42b6ed040d426c939f7657d4bc991b8
Kennedy Lodonu <Kenny-Rogers@users.noreply.github.com>
2022-10-29 22:35:37 +0000

    Update StarNotary.sol
    Added app to the path to import the token file.
    Fixes issue  #28

--- contracts/StarNotary.sol
+++ contracts/StarNotary.sol
@@ -1,7 +1,7 @@
 pragma solidity >=0.4.24;
 
 //Importing openzeppelin-solidity ERC-721 implemented Standard
-import "../node_modules/openzeppelin-solidity/contracts/token/ERC721/ERC721.sol";
+import "../app/node_modules/openzeppelin-solidity/contracts/token/ERC721/ERC721.sol";
 
 // StarNotary Contract declaration inheritance the ERC721 openzeppelin implementation
 contract StarNotary is ERC721 {
